0a25c4249a28ab5c9d9182643dfa9b3ba564aa1c
zonko16 <50302758+zonko16@users.noreply.github.com>
2019-06-14 13:07:09 +0200

    Update deploy.sh

--- deploy_script/deploy.sh
+++ deploy_script/deploy.sh
@@ -157,7 +157,7 @@ fi
 
 # Page 2 scripts setup
 echo "Do you want to configure the Scripts page? "
-read -e -n 1 -p "(y/n): -i "" input_p2_conf
+read -e -n 1 -p "(y/n)": -i "" input_p2_conf
 if [ "$input_p2_conf" == "y"]
 then
   echo "================================================="
